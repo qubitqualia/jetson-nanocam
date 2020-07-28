11eabfc4dc02d10c060eba0f7e53325e3953b80b
qubitqualia <39470632+qubitqualia@users.noreply.github.com>
2020-07-28 16:14:02 -0400

    Delete media_client.py

--- media_client.py
+++
@@ -1,9 +0,0 @@
-from nanocam import mediaclient
-
-client = mediaclient.MediaClient()
-client.set_hostip('192.168.1.168')
-client.set_msg_port(7155)
-client.connect()
-
-client.image_request(5, 1, display=True)
-
